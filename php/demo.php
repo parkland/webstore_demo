349e4832bfafd1d4712b789842cd5e4ffd83c18e
Charles Schultz <sacrophyte@gmail.com>
2017-06-21 12:22:05 -0500

    Update demo.php

--- php/demo.php
+++ php/demo.php
@@ -1,9 +1,8 @@
 <?php
-
 define('INCLUDE_CHECK',1);
 require "connect.php";
 require "load_sql.php";
-
+require "get_products.php";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -46,24 +45,7 @@ require "load_sql.php";
     
     		<div class="content drag-desired">
             	
-                <?php
-
-		   $query = $sqls['select_10_products'];
-		   if ($result = $mysqli->query($query)) {
-
-                        /* fetch associative array */
-                        while ($row = $result->fetch_assoc()) {
-			    $image = "no-image-icon-15.png";
-			    if ((isset($row['imagepath']) && trim($row['imagepath'])!=='')) {
-				     $image = $row['imagepath'];
-			    }
-                            echo '<div class="product"><img src="../img/'.$image.'" alt="'.htmlspecialchars($row['name']).'" width="128" height="128" class="pngfix" /></div>';
-                        }
-
-                        /* free result set */
-                        $result->free();
-		   }
-		?>
+                <?php get_products($mysqli, $sqls) ?>
                 
                 
        	        <div class="clear"></div>
