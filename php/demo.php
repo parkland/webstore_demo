dab44f766cd4e54cb511a3208b012464d660fd02
Charles Schultz <sacrophyte@gmail.com>
2017-06-21 11:29:52 -0500

    Update demo.php

--- php/demo.php
+++ php/demo.php
@@ -54,7 +54,7 @@ require "load_sql.php";
                         /* fetch associative array */
                         while ($row = $result->fetch_assoc()) {
 			    $image = "no-image-icon-15.png";
-			    if ((isset($row['imagepath']) && trim($row['imagepath'])!==='')) {
+			    if ((isset($row['imagepath']) && trim($row['imagepath'])!=='')) {
 				     $image = $row['imagepath'];
 			    }
                             echo '<div class="product"><img src="../img/'.$image.'" alt="'.htmlspecialchars($row['name']).'" width="128" height="128" class="pngfix" /></div>';
