1e11351053555c8e6bf85191d8cf4c1fd8a8d35e
Charles Schultz <sacrophyte@gmail.com>
2017-06-21 12:32:45 -0500

    Update get_products.php

--- php/get_products.php
+++ php/get_products.php
@@ -8,7 +8,7 @@ function get_products($mysqli, $sqls) {
     while ($row = $result->fetch_assoc()) {
       $image = "no-image-icon-15.png";
       if ((isset($row['imagepath']) && trim($row['imagepath'])!=='')) {
-        $image = $row['imagepath'];
+        $image = "products/".$row['imagepath'];
       }
       echo '<div class="product"><img src="../img/'.$image.'" alt="'.htmlspecialchars($row['name']).'" width="128" height="128" class="pngfix" /></div>';
     }
