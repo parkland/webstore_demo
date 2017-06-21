143c3b0e1d539a0107ba1fb70a67abfe596b4782
Charles Schultz <sacrophyte@gmail.com>
2017-06-21 11:09:43 -0500

    Update load_sql.php

--- php/load_sql.php
+++ php/load_sql.php
@@ -2,9 +2,9 @@
 /* Finds all the *.sql files in the sql directory, and loads the contents into an associative array */
 /* This is to allow students to write individual sql statements into each file (one file per query) */
 
-$sqls = [];
+$sqls = array();
 foreach (glob("../sql/*.sql") as $FQfilename) {
-    $filename = basename($FQfilename)
+    $filename = basename($FQfilename, ".sql");
     $sqls[$filename] = file_get_contents($FQfilename);
 }
 
